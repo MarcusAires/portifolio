9b08bab4f95c1660328bc37cf37307aa666bd664
Marcus Gadelha <gadelhamarcus27@gmail.com>
2025-02-22 19:01:16 -0300

    cards refatorados e reconstruidos, ainda falta adicionar icone e editar, titulos, descrição e adicionar place holders

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,17 +1,44 @@
 import React from "react";
 import FullScreenSection from "./FullScreenSection";
-import { CardHeader, CardFooter, CardBody, Heading, Card, SimpleGrid,Image,Stack, Text} from "@chakra-ui/react";
-//import Card from "./Card";
+import { CardHeader, CardFooter, CardBody, Heading, Card, SimpleGrid,Image,Stack, Text, Divider, ButtonGroup, Button, Icon} from "@chakra-ui/react";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+
 
 const projects = [
   {
     title: "Lista de compras",
     description:
       "Um CRUD simples que usa apenas HTML, CSS e javascript para criar uma lista de compras com modal, responsividade e localstorage",
-    getImageSrc: () => require("../images/gifcrud.gif"),
     url: "https://lista-de-compras-mercado-easy.vercel.app/",
-    image: '/src/images/gifcrud.gif'
+    img: require('../images/gifcrud.gif'),
+    repo: 'https://github.com/MarcusAires/projeto_lista_de_compras.git'
   },
+  {
+    title: "Lista de compras",
+    description:
+      "Um CRUD simples que usa apenas HTML, CSS e javascript para criar uma lista de compras com modal, responsividade e localstorage",
+    url: "https://lista-de-compras-mercado-easy.vercel.app/",
+    img: require('../images/gifcrud.gif'),
+    repo: 'https://github.com/MarcusAires/projeto_lista_de_compras.git'
+  },
+  {
+    title: "Lista de compras",
+    description:
+      "Um CRUD simples que usa apenas HTML, CSS e javascript para criar uma lista de compras com modal, responsividade e localstorage",
+    url: "https://lista-de-compras-mercado-easy.vercel.app/",
+    img: require('../images/gifcrud.gif'),
+    repo: 'https://github.com/MarcusAires/projeto_lista_de_compras.git'
+  },
+  {
+    title: "Lista de compras",
+    description:
+      "Um CRUD simples que usa apenas HTML, CSS e javascript para criar uma lista de compras com modal, responsividade e localstorage",
+    url: "https://lista-de-compras-mercado-easy.vercel.app/",
+    img: require('../images/gifcrud.gif'),
+    repo: 'https://github.com/MarcusAires/projeto_lista_de_compras.git'
+  },
+
 ];
 
 const ProjectsSection = () => {
@@ -26,12 +53,12 @@ const ProjectsSection = () => {
       <Heading as="h1" id="projects-section">
         Meus projetos
       </Heading>
-      <SimpleGrid>
+      <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px,1fr))'>
         {projects.map((project) => (
           <Card maxW='sm'>
             <CardBody>
             <Image
-            src={project.image}
+            src={project.img}
             alt={project.title}
             borderRadius='lg'
             />
@@ -42,6 +69,21 @@ const ProjectsSection = () => {
               </Text>
             </Stack>
             </CardBody>
+            <Divider/>
+            <CardFooter>
+              <ButtonGroup spacing='2'>
+              <Button variant='solid' colorScheme='blue'>
+               <a href={project.url} target="_blank">
+                Visite a página 
+                </a> 
+              </Button>
+              <Button variant='ghost' colorScheme="blue">
+                <a href={project.repo} target='_blank'>
+                Confira o código
+                </a>
+              </Button>
+              </ButtonGroup>
+            </CardFooter>
           </Card>
         ))}
         </SimpleGrid>
